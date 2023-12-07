9136483ec779a9519070ad2393c8ab85c890207a
deking254 <dekingsky@gmail.com>
2023-12-07 12:08:59 +0000

    added the union to the second argument of the function

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """multiple possible annotations for a variable"""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int or float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns tuple with k and square of v as float"""
     return tuple((k, v**2))
