f23d6b187d58229d20a52cb1f6d8554f1a7e4b33
deking254 <dekingsky@gmail.com>
2023-12-11 19:52:35 +0000

    fixed the pycodestyle issues

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """measuring  the time it takes to excecute a coroutine"""
-wait_n = __import__('1-concurrent_coroutines').wait_n
 import time
 import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
-def measure_time(n: int, max_delay: int) ->float:
+
+def measure_time(n: int, max_delay: int) -> float:
     """returns a float that reps time of excecution"""
     start_time: float = time.perf_counter()
     asyncio.run(wait_n(n,  max_delay))
