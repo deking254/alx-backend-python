f52b27a3edb1e7d4a9fc04023caa818671f6b406
deking254 <dekingsky@gmail.com>
2023-12-11 12:03:56 +0000

    added the comments to the function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,9 +3,11 @@
 
 import random
 import asyncio
+
+
 async def wait_random(max_delay=10):
+    """a coroutine that returns a random number"""
     wait = random.random()
     if (wait <= max_delay):
-        """await asyncio.wait_for(500, 5000)"""
         await asyncio.sleep(wait)
         return wait;
