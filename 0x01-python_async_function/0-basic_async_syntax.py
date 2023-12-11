29608625c486d69580b32357458408fa45a3c497
deking254 <dekingsky@gmail.com>
2023-12-11 13:13:08 +0000

    tsting withouta return

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,4 +9,3 @@ async def wait_random(max_delay: float=10) -> float:
     """a coroutine that returns a random number"""
     wait: float = random.uniform(0, max_delay)
     await asyncio.sleep(wait)
-    return wait
