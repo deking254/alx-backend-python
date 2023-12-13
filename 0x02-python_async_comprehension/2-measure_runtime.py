401a979bd508e0e2f0d98c1cf15ba523d1ad6352
deking254 <dekingsky@gmail.com>
2023-12-13 12:53:22 +0000

    added code to 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""creating four parallel comprehensions"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """execute async_comprehension four times in parallel"""
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    stop_time = time.perf_counter()
+    return stop_time - start_time
