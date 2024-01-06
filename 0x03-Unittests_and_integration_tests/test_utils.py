919abc68c55ad2411b85121cd3a9b6e3f9a954e6
deking254 <dekingsky@gmail.com>
2024-01-06 13:59:49 +0000

    fixed pycode

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,12 +8,17 @@ from typing import (
                     Sequence,
                     Any
                    )
+
+
 class parameterize():
+    """it whatever it is"""
     def expand(method) -> Any:
         """extends the inputsi"""
         def para(self, nested_map: Mapping, path: Sequence):
+            """nitty gritty"""
             method(nested_map, path)
         return para
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -21,6 +26,8 @@ class TestAccessNestedMap(unittest.TestCase):
     utils.access_nested_map is working as expected
     """
     @parameterize.expand
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence) -> Any:
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence
+                               ) -> Any:
         """checks the input for validity"""
         map(unittest.TestCase.assertEqual, nested_map, path)
