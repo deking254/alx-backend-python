9f9ad22618380458635e12e95ec16f6e0ed5ad6c
deking254 <dekingsky@gmail.com>
2024-01-06 13:28:53 +0000

    param changes

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,21 +8,20 @@ from typing import (
                     Sequence,
                     Any
                    )
-
-
-def parameterized(method: Callable) -> Callable:
-    """checks for th inputs validity"""
-    def expand(self, nested_map, path) -> Any:
-        """extends the inputs"""
-        map(unittest.TestCase.assertEqual, nested_map, path)
-    return expand
-
+class parameterize():
+    def expand(method) -> Any:
+        """extends the inputsi"""
+        def para(self, nested_map: Mapping, path: Sequence):
+            result = method(self, nested_map, path)
+            map(unittest.TestCase.assertEqual, nested_map, path)
+        return para
 
 class TestAccessNestedMap(unittest.TestCase):
     """
     implements methods to check
     utils.access_nested_map is working as expected
     """
-    @parameterized.expand
+    @parameterize.expand
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence) -> Any:
         """checks the input for validity"""
+        return utils.access_nested_map(nested_map, path)
