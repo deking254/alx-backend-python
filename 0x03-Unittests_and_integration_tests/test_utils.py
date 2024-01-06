792cb9937a54398ae5914fe5a30a6f3d65bb7cf9
deking254 <dekingsky@gmail.com>
2024-01-06 14:15:06 +0000

    took a whole new different approach

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,14 +10,17 @@ from typing import (
                    )
 
 
-class parameterize():
-    """it whatever it is"""
-    def expand(method) -> Any:
-        """extends the inputsi"""
-        def para(self, nested_map: Mapping, path: Sequence):
-            """nitty gritty"""
-            method(nested_map, path)
-        return para
+def expand(method) -> Any:
+    """extends the inputsi"""
+    def para(self, nested_map: Mapping, path: Sequence):
+        """nitty gritty"""
+        test1 = utils.access_nested_map({"a": 1}, ("a",))
+        test2 = utils.access_nested_map({"a": {"b": 2}}, ("a",))
+        test3 = utils.access_nested_map({"a": {"b": 2}}, ("a", "b"))
+        unittest.TestCase.assertEqual(test1, 1)
+        unittest.TestCase.assertEqual(test2, {"b": 2})
+        unittest.TestCase.assertEqual(test3, 2)
+    return para
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,9 +28,9 @@ class TestAccessNestedMap(unittest.TestCase):
     implements methods to check
     utils.access_nested_map is working as expected
     """
-    @parameterize.expand
+    @expand
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence
                                ) -> Any:
         """checks the input for validity"""
-        map(unittest.TestCase.assertEqual, nested_map, path)
+        pass
