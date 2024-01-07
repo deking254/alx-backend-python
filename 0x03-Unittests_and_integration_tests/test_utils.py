c7b6cd96353b0c7efb66bf022cef6b650f46cdfb
deking254 <dekingsky@gmail.com>
2024-01-07 06:13:51 +0000

    used actual parameterized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """testing the utitls file"""
 import unittest
 import utils
+from parameterized import parameterized
 from typing import (
                     Callable,
                     Mapping,
@@ -9,29 +10,13 @@ from typing import (
                     Any
                    )
 
-class parameterized():
-
-    def expand(method) -> Any:
-        """extends the inputsi"""
-        def para(self, nested_map: Mapping, path: Sequence):
-            """nitty gritty"""
-            test1 = utils.access_nested_map({"a": 1}, ("a",))
-            test2 = utils.access_nested_map({"a": {"b": 2}}, ("a",))
-            test3 = utils.access_nested_map({"a": {"b": 2}}, ("a", "b"))
-            unittest.TestCase.assertEqual(test1, 1)
-            unittest.TestCase.assertEqual(test2, {"b": 2})
-            unittest.TestCase.assertEqual(test3, 2)
-    return para
-
-
 class TestAccessNestedMap(unittest.TestCase):
     """
     implements methods to check
     utils.access_nested_map is working as expected
     """
-    @parameterized.expand
-    def test_access_nested_map(self, nested_map: Mapping,
-                               path: Sequence
-                               ) -> Any:
+    @parameterized.expand([({"a": 1}, ("a",)), ({"a": {"b": 2}},("a",)), ({"a": {"b": 2}},("a", "b"))])
+    def test_access_nested_map(name, input, expected) -> Any:
         """checks the input for validity"""
-        pass
+        actual = utils.access_nested_map(input)
+        unittest.TestCase.assertEqual(actual, expected)
