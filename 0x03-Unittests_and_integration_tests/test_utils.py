cc1d9922d835f0630e8e67a64e3ac1d26fe0017e
deking254 <dekingsky@gmail.com>
2024-01-07 07:41:37 +0000

    this has to be it

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,8 +15,8 @@ class TestAccessNestedMap(unittest.TestCase):
     implements methods to check
     utils.access_nested_map is working as expected
     """
-    @parameterized.expand([({"a": 1}, ("a",)), ({"a": {"b": 2}},("a",)), ({"a": {"b": 2}},("a", "b"))])
-    def test_access_nested_map(name, input, expected) -> Any:
+    @parameterized.expand([({"a": 1}, ("a",), 1), ({"a": {"b": 2}}, ("a",), {"b": 2}), ({"a": {"b": 2}}, ("a", "b"), 2)])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any) -> Any:
         """checks the input for validity"""
-        actual = utils.access_nested_map(input)
-        unittest.TestCase.assertEqual(actual, expected)
+        actual = utils.access_nested_map(nested_map, path)
+        unittest.TestCase.assertEqual(self, actual, expected)
