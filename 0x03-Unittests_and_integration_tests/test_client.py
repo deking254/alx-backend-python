51f26ad3831ac77851d245029d088588d5582afc
deking254 <dekingsky@gmail.com>
2024-01-10 07:28:14 +0000

    added a the get_json mock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""this is a module with a series of tests relatig to client side"""
+from unittest import TestCase, mock, main
+from parameterized import parameterized
+import client
+
+
+class TestGithubOrgClient(TestCase):
+    """testing the that the githuborgclient works as expected"""
+    @parameterized.expand(
+            [(('google'), 'https://api.github.com/orgs/google'),
+                (('abc'), 'https://api.github.com/orgs/abc')])
+    def test_org(self, company, expected_url):
+        """testing the org function in client file"""
+        with mock.patch('client.get_json') as mock_org:
+            client_instance = client.GithubOrgClient(company)
+            a = client_instance.org
+            mock_org.assert_called_once_with(expected_url)
+if __name__ == '__main__':
+    main()
