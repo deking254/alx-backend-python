7fe8289ee0f7658f0e7db09e1dbc2b945813240b
deking254 <dekingsky@gmail.com>
2024-01-10 14:46:12 +0000

    fixd the pycodestyle errors

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,13 +12,17 @@ class TestGithubOrgClient(TestCase):
                 (('abc'), 'https://api.github.com/orgs/abc')])
     def test_org(self, company, expected_url):
         """testing the org function in client file"""
-        with mock.patch('client.get_json', return_value = {"repos_url": expected_url}) as mock_get_json:
+        with mock.patch('client.get_json',
+                        return_value={"repos_url": expected_url})
+        as mock_get_json:
             client_instance = client.GithubOrgClient(company)
             client_instance.org
             mock_get_json.assert_called_once_with(expected_url)
+
     def test_public_repos_url(self):
         """method to unit-test GithubOrgClient._public_url"""
-        with mock.patch('client.GithubOrgClient.org', new_callable=mock.PropertyMock) as mock_org:
+        with mock.patch('client.GithubOrgClient.org',
+                        new_callable=mock.PropertyMock) as mock_org:
             mock_org.return_value = {"repos_url": "google.your"}
             a = client.GithubOrgClient('google')._public_repos_url
             self.assertEqual(a, "google.your")
@@ -27,12 +31,14 @@ class TestGithubOrgClient(TestCase):
     def test_public_repos(self, mock_get_json):
         """testing the public repos func works as expected"""
         mock_get_json.return_value = [{"name": 'https://github.com/deking254'}]
-        with mock.patch('client.GithubOrgClient._public_repos_url', new_callable=mock.PropertyMock) as mock_public_repos:
+        with mock.patch('client.GithubOrgClient._public_repos_url',
+                        new_callable=mock.PropertyMock) as mock_public_repos:
             mock_public_repos.return_value = 'https://github.com/deking254'
             a = client.GithubOrgClient('google').public_repos()
             self.assertEqual(a, [mock_public_repos.return_value])
             mock_public_repos.assert_called_once()
         mock_get_json.assert_called_once()
+
 
 if __name__ == '__main__':
     main()
