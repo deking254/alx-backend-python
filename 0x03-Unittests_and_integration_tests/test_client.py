957d6dcef6708db2be8f8db12360b9a744a8edbe
deking254 <dekingsky@gmail.com>
2024-01-10 13:48:15 +0000

    added the property mock file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,9 +12,16 @@ class TestGithubOrgClient(TestCase):
                 (('abc'), 'https://api.github.com/orgs/abc')])
     def test_org(self, company, expected_url):
         """testing the org function in client file"""
-        with mock.patch('client.get_json') as mock_org:
+        with mock.patch('client.get_json', return_value = {"repos_url": expected_url}) as mock_get_json:
             client_instance = client.GithubOrgClient(company)
-            a = client_instance.org
-            mock_org.assert_called_once_with(expected_url)
+            client_instance.org
+            mock_get_json.assert_called_once_with(expected_url)
+    def test_public_repos_url(self):
+        """method to unit-test GithubOrgClient._public_url"""
+        with mock.patch('client.GithubOrgClient.org', new_callable=mock.PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": "google.your"}
+            a = client.GithubOrgClient('google')._public_repos_url
+            self.assertEqual(a, "google.your")
+
 if __name__ == '__main__':
     main()
